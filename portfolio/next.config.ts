9741b2af69619ddafbaffaf3bb0e50f98b7651bf
stevetheghost <steven.wojtan.15@cnu.edu>
2025-02-12 19:40:33 -0500

    remove lots of template code

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/portfolio",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
